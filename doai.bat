b3b5ca4a9f745f7460cf3c8b74ea6a57dce9f176
Bubble-Big <747269289@qq.com>
2026-08-09 03:37:47 +0800

    更新 README 仓库地址，移除废弃脚本

--- doai.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-d:
-cd D:\ClaudeProject\VideoCourseAI-main
-docker-compose up -d
-cd client
-npm run dev
--- stop.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-d:
-cd /d D:\ClaudeProject\VideoCourseAI-main
-docker-compose down
-taskkill /f /im node.exe >nul 2>&1
